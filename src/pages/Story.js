b2a299742275ef829dc8bba2c8264353661e57cd
Rodrigo Ferrari <rodrigo@MacBook-Pro.local>
2019-11-29 16:08:24 +1300

    Single mapstory

--- src/pages/Story.js
+++ src/pages/Story.js
@@ -1,8 +1,44 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { API, graphqlOperation } from "aws-amplify";
+import { getMapstory } from "../graphql/queries";
 
 class Story extends Component {
+  state = {
+    mapstory: null,
+    isLoading: true
+  };
+
+  componentDidMount() {
+    this.handleGetMapstory();
+  }
+
+  handleGetMapstory = async () => {
+    const input = {
+      id: this.props.storyId
+    };
+    const result = await API.graphql(graphqlOperation(getMapstory, input));
+    console.log(result);
+    this.setState({
+      mapstory: result.data.getMapstory,
+      isLoading: false
+    });
+  };
+
   render() {
-    return <h1>Story {this.props.storyId}</h1>;
+    const { mapstory, isLoading } = this.state;
+
+    if (isLoading) {
+      return <p>Loading</p>;
+    }
+
+    return (
+      <>
+        <Link to="/">back to Mapstories list</Link>
+        <p>{mapstory.title}</p>
+        <p>{mapstory.description}</p>
+      </>
+    );
   }
 }
 
